6a24ce7d63f3dcbc8ab859fc12b6905b08276c45
ThomasBeyney <thomas.beyney@ynov.com>
2026-03-30 14:45:43 +0200

    Navbar test

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+function NavBar() {
+    return (
+        <nav className="main-nav">
+            <ul className="nav-list">
+                <li className="nav-list-item">
+                    <a className="nav-link">
+                        <p> Test Page </p>
+                    </a>
+                </li>
+                <li className="nav-list-item">
+                    <a className="nav-link">
+                        <img />
+                    </a>
+                </li>
+                <li className="nav-list-item">
+                    <a className="nav-link">
+                        <img />
+                    </a>
+                </li>
+                <li className="nav-list-item">
+                    <a className="nav-link">
+                        <img />
+                    </a>
+                </li>
+                <li className="nav-list-item">
+                    <a className="nav-link">
+                        <img />
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar;
